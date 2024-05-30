d032c6c32ddd4801838706f650a25cfaf4273f05
jx-dohwan <jx7789@gmail.com>
2024-05-30 10:05:44 +0900

    feat.#6.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,14 +1,48 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+const phoneSchema = z
+    .string()
+    .trim()
+    .refine(
+        (phone) => validator.isMobilePhone(phone, "ko-KR"),
+        "Wrong phone format"
+    );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
+interface ActionState {
+    token: boolean;
+}
 
-export async function smsLogIn(prevState:any, formData:FormData) {
-    console.log(formData.get("token"));
-    console.log(tokenSchema.parse(formData.get("token")))
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+    const phone = formData.get("phone");
+    const token = formData.get("token");
+    if (!prevState.token) {
+        const result = phoneSchema.safeParse(phone);
+        if (!result.success) {
+            return {
+                token: false,
+                error: result.error.flatten(),
+            };
+        } else {
+            return {
+                token: true,
+            };
+        }
+    } else {
+        const result = tokenSchema.safeParse(token);
+        if (!result.success) {
+            return {
+                token: true,
+                error: result.error.flatten(),
+            };
+        } else {
+            redirect("/")
+        }
+    }
 }
