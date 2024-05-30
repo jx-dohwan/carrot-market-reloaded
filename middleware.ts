0c1b99b641342d1d7f1922673d61df92716acb6c
jx-dohwan <jx7789@gmail.com>
2024-05-30 15:37:35 +0900

    feat.#8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,33 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+    [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+    "/": true,
+    "/login": true,
+    "/sms": true,
+    "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("hello");
+    const session = await getSession();
+    const exists = publicOnlyUrls[request.nextUrl.pathname];
+    if (!session.id) {
+        if (!exists) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    } else {
+        if (exists) {
+            return NextResponse.redirect(new URL("/products", request.url));
+        }
+    }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
 // 웹사이트의 모든 request하나마다 middleware가 실행된다.
