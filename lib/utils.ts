d06cfdc98f32bae22a43e09255b5c3187bc665ff
jx-dohwan <jx7789@gmail.com>
2024-05-31 09:55:57 +0900

    feat.#10.4 Detail Skeleton

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,14 @@
-import getSession from "./session";
-
-
-export const LogIn = async (id: number) => {
-    const session = await getSession();
-    session.id = id;
-    await session.save();
-
-}
+export function formatToTimeAgo(date: string): string {
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const time = new Date(date).getTime();
+    const now = new Date().getTime();
+    const diff = Math.round((time - now) / dayInMs);
+  
+    const formatter = new Intl.RelativeTimeFormat("ko");
+  
+    return formatter.format(diff, "days");
+  }
+  
+  export function formatToWon(price: number): string {
+    return price.toLocaleString("ko-KR");
+  }
