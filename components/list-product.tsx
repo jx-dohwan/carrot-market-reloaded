205956c013d07df47a99d643811bca5766a76b87
jx-dohwan <jx7789@gmail.com>
2024-05-31 11:46:28 +0900

    feat.#10.7 Pagination Actions

--- components/list-product.tsx
+++ components/list-product.tsx
@@ -20,14 +20,14 @@ export default function ListProduct({
     return (
         <Link href={`/products/${id}`} className="flex gap-5">
             <div className="relative size-28 rounded-md overflow-hidden">
-                <Image fill src={photo} alt={title} />
+                <Image fill src={photo} className="object-cover" alt={title} />
             </div>
             <div className="flex flex-col gap-1 *:text-white">
                 <span className="text-lg">
                     {title}
                 </span>
                 <span className="text-sm text-neutral-500">
-                {formatToTimeAgo(created_at.toString())}
+                    {formatToTimeAgo(created_at.toString())}
                 </span>
                 <span className="text-lg font-semibold">{formatToWon(price)}원</span>
             </div>
