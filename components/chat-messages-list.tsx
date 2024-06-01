3b85f287a66fc6b30b805ece68164f0289741fde
jx-dohwan <jx7789@gmail.com>
2024-06-01 22:53:41 +0900

    feat.#15.5 Supabase Broadcast

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -3,9 +3,9 @@
 import { InitialChatMessages } from "@/app/chats/[id]/page";
 import { formatToTimeAgo } from "@/lib/utils";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
-import { createClient } from "@supabase/supabase-js";
+import { RealtimeChannel, createClient } from "@supabase/supabase-js";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const SUPABASE_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqYnZ2a3B3ZXZ0bm9jdG5vcnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyNDk0MjcsImV4cCI6MjAzMjgyNTQyN30.-fnkX3fg3hBpM96M9BwRsYaEgfciiJIHleuZX6rT2ss";
@@ -23,6 +23,7 @@ export default function ChatMessagesList({
 }: ChatMessageListProps) {
     const [messages, setMessages] = useState(initialMessages);
     const [message, setMessage] = useState("");
+    const channel = useRef<RealtimeChannel>();
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const {
             target: { value },
@@ -44,14 +45,24 @@ export default function ChatMessagesList({
                 },
             },
         ])
+        channel.current?.send({
+            type: "broadcast",
+            event: "message",
+            payload: { message },
+        });
         setMessage("");
     };
     useEffect(() => {
         const client = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
-        const channel = client.channel(`room-${chatRoomId}`);
-        channel.on("broadcast", { event: "message" }, (payload) => {
-            console.log(payload);
-        });
+        channel.current = client.channel(`room-${chatRoomId}`);
+        channel.current
+            .on("broadcast", { event: "message" }, (payload) => {
+                console.log(payload);
+            })
+            .subscribe();
+        return () => {
+            channel.current?.unsubscribe();
+        };
     }, [chatRoomId]);
     return (
         <div className="p-5 flex flex-col gap-5 min-h-screen justify-end">
