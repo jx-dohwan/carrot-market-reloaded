fffdc22ac582048c7521846307f3dfb5aab93dc1
jx-dohwan <jx7789@gmail.com>
2024-06-11 21:46:08 +0900

    feat.테스트2-기존의 main코드를 수정후 나중에 만든 new branch로 업로드

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <span className="text-9xl">🥕</span>
         <h1 className="text-4xl ">당근</h1>
-        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+        <h2 className="text-2xl">당근 마겟에 어서오세요!-테스트중</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
         <Link href="/create-account" className="primary-btn text-lg py-2.5">
